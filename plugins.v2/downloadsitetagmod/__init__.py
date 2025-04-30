c31ac080c29f4469ab57d66a929a4e416fa39a69
qiaoyun680 <549653222@qq.com>
2025-04-30 09:23:54 +0800

    下载任务分类与标签魔改版:更新目录名称

--- plugins.v2/downloadsitetagmod/__init__.py
+++ plugins.v2/downloadsitetagmod/__init__.py
@@ -29,7 +29,7 @@ class DownloadSiteTagMod(_PluginBase):
     # 插件图标
     plugin_icon = "Youtube-dl_B.png"
     # 插件版本
-    plugin_version = "2.3.2"
+    plugin_version = "2.3.2.1"
     # 插件作者
     plugin_author = "叮叮当,Seed680"
     # 作者主页
