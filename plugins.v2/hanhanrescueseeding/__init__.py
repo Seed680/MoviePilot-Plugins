1b01115a6d32138b8778a0f474431916c2542e62
noone <noone@email.com>
2025-08-18 17:08:49 +0800

    憨憨保种区:0.0.5

--- plugins.v2/hanhanrescueseeding/__init__.py
+++ plugins.v2/hanhanrescueseeding/__init__.py
@@ -27,7 +27,7 @@ class HanHanRescueSeeding(_PluginBase):
     # 插件图标
     plugin_icon = "hanhan.png"
     # 插件版本
-    plugin_version = "0.0.4"
+    plugin_version = "0.0.5"
     # 插件作者
     plugin_author = "Seed"
     # 作者主页
