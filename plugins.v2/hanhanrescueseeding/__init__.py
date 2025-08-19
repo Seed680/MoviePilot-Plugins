c890f0a6a933996ef5ca96c7d661ee52d7a9098c
noone <noone@email.com>
2025-08-19 14:54:01 +0800

    bugfix

--- plugins.v2/hanhanrescueseeding/__init__.py
+++ plugins.v2/hanhanrescueseeding/__init__.py
@@ -27,7 +27,7 @@ class HanHanRescueSeeding(_PluginBase):
     # 插件图标
     plugin_icon = "hanhan.png"
     # 插件版本
-    plugin_version = "1.1.8.1"
+    plugin_version = "1.1.8.2"
     # 插件作者
     plugin_author = "Seed"
     # 作者主页
