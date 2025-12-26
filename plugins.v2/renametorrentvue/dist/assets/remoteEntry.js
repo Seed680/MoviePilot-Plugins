e655ab8196f899ba2a5169e8a8c907cc7d21679d
noone <noone@noone.com>
2025-12-26 15:10:46 +0800

    chore(build): 更新插件版本并优化前端提示
    - 更新重命名种子Vue版插件版本从1.0.2到1.0.2.1
    - 更新下载路径格式化插件版本从1.0.1到1.0.1.1
    - 在package.v2.json中添加新版本历史记录
    - 移除旧的前端组件文件并生成新的组件文件
    - 优化前端用户界面提示功能

--- plugins.v2/renametorrentvue/dist/assets/remoteEntry.js
+++ plugins.v2/renametorrentvue/dist/assets/remoteEntry.js
@@ -5,8 +5,8 @@ const currentImports = {};
       dynamicLoadingCss([], false, './Config');
       return __federation_import('./__federation_expose_Config-UXlpXG_V.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},
 "./Page":()=>{
-      dynamicLoadingCss(["__federation_expose_Page-Djv0HPn5.css"], false, './Page');
-      return __federation_import('./__federation_expose_Page-Fl_fxljJ.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["__federation_expose_Page-Cbd20m89.css"], false, './Page');
+      return __federation_import('./__federation_expose_Page-CTS5HXMD.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
